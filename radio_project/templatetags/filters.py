a7412b29da91bd0d877cbd2a04e53726ada5117c
Vin <bevin@jabana.se>
2012-09-15 23:48:19 +0200

    Added a ton of static image resources Added Radvars model (id doesn't have auto_incr?) Finished home view/template Added news filter

--- radio_project/templatetags/filters.py
+++ radio_project/templatetags/filters.py
@@ -18,3 +18,15 @@ def searchtimeformat(value, arg):
     """Formats a datetime object for the Search page"""
     return tools.search_time_format(value)
 
+@register.filter
+def newsformat(value, arg):
+    """Formats a news post string with line breaks and truncation (if needed)
+    Argument: truncate - truncates the string at the TRUNCATE mark
+    """
+    value = value.replace('\n', '<br>')
+    if 'TRUNCATE' in value:
+        if arg == 'truncate':
+            value = value[:value.index('TRUNCATE')]
+        value = value.replace('TRUNCATE', '')
+    return value
+    
